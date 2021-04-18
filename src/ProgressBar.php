f3756b2e096e2141317d900842c7acd0d9a739f9
kyoya0819 <kyoya0819@gmail.com>
2021-04-18 19:08:52 +0900

    Initial commit

---
+++ src/ProgressBar.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace kyoya0819\ProgressBar;
+
+use Exception;
+
+final class ProgressBar
+{
+    private int $width;
+    private int $info_max_length;
+    private string $char;
+
+
+    public function __construct(int $width = 50, string $char = "=")
+    {
+        $this->width = $width;
+        $this->info_max_length = 0;
+        $this->char = $char;
+    }
+
+
+    /**
+     * @param string $variable_name
+     * @param string $value
+     * @return ProgressBar
+     * @throws Exception
+     */
+    public function update(string $variable_name, string $value): ProgressBar
+    {
+        $accepts = ['width', 'char'];
+
+        if (!in_array($variable_name, $accepts))
+            throw new Exception('The specified variable_name is not found.');
+
+        if ($variable_name === 'width')
+            $this->width = (int) $value;
+
+        if ($variable_name === 'char')
+            $this->char = $value;
+
+        return $this;
+    }
+
+
+    /**
+     * @param int $done
+     * @param int $total
+     * @param string $info
+     * @return string
+     */
+    public function view(int $done, int $total, string $info = ""): string
+    {
+        $percent = round($done / $total * 100);
+        $bar = round($this->width * $percent / 100);
+
+
+        if (mb_strlen($info) > $this->info_max_length)
+            $this->info_max_length = mb_strlen($info);
+
+
+        $percent = str_pad($percent, 3, " ", STR_PAD_LEFT) . '%';
+        $progress = str_repeat("=", $bar);
+        $progress_blank = str_repeat(" ", $this->width - $bar);
+        $info_blank = str_repeat(" ", $this->info_max_length - mb_strlen($info));
+
+
+        return sprintf("%s [%s>%s] %s/%s %s %s\r", $percent, $progress, $progress_blank, $done, $total, $info, $info_blank);
+    }
+}
